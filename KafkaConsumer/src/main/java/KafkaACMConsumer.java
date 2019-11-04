8fa497744a0a3940f7cbc8048cd3b0662020bffc
aliahaleem789 <ahaleem@ellisdon.com>
2019-11-04 15:09:17 -0500

    KafkaConsumer  connect to Topics and monitor Projects table

--- KafkaConsumer/src/main/java/KafkaACMConsumer.java
+++ KafkaConsumer/src/main/java/KafkaACMConsumer.java
@@ -8,23 +8,18 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.Serde;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.streams.StreamsConfig;
-
-import java.util.Arrays;
-import java.util.Properties;
 
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 //http://10.104.120.22:8081
 
 
-public class KafkaConsumer {
+public class KafkaACMConsumer{
 
-    private final static String TOPIC = "acm-streams-pipe";
+    private final static String TOPIC = "acm-test-consumer";
     private final static String BOOTSTRAP_SERVERS = "kafka-broker-0.c.radix-shared-inf.internal:9092, kafka-broker-1.c.radix-shared-inf.internal:9092, kafka-broker-2.c.radix-shared-inf.internal:9092";//"localhost:9092,localhost:9093,localhost:9094";
     private final static String KEY_CONNECT_NAME = "projectService.cust_ellisdon.project.Key";
 
@@ -32,22 +27,18 @@ public class KafkaConsumer {
 
 
         Properties props = new Properties();
-
-        // props.put(StreamsConfig.APPLICATION_ID_CONFIG, TOPIC);
-        props.put("group.id", "testConsumer");
-        //props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put("group.id", TOPIC);
         props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
-
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
 
         props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://10.104.120.22:8081");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        // props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
 
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        //props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        //props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
 
         final Serde<String> stringSerde = Serdes.String();
